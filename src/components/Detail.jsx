84a25de9ba795124ccd512042f4c8d8dbbcb2a45
unknown <rontab68@yahoo.com>
2021-08-17 05:47:15 -0700

    updated

---
+++ src/components/Detail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function Detail(props)
+{
+return(
+<React.Fragment>
+<p className="info">{props.detailInfo}</p>
+</React.Fragment>
+)
+    
+}
+
+export default Detail;
